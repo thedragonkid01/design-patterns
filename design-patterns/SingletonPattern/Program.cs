db2704846a14940178452718396f6d3af2019b74
LAPTOP-QGUA4V9E\tamki <tamkid2408@gmail.com>
2023-02-07 14:53:44 +0700

    Implement singleton pattern

---
+++ design-patterns/SingletonPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton objA = Singleton.GetInstance();
+            Singleton objB = Singleton.GetInstance();
+
+            if (objA != objB)
+            {
+                Console.WriteLine("Not same");
+            }
+            else
+            {
+                Console.WriteLine("Both are same");
+            }
+        }
+    }
+}
